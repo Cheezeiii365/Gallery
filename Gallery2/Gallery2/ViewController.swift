d3638d9fbbf03cf75376d50176dd15f28f13fbf8
BernieOdoy004 <cheezeiii365@gmail.com>
2018-06-17 05:28:27 -0400

    Initial commit

---
+++ Gallery2/Gallery2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gallery2
+//
+//  Created by Bernie Odoy on 6/17/18.
+//  Copyright © 2018 Bernie Odoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
